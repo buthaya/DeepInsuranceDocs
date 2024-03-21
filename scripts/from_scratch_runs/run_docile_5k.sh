4043d7c4401013fc58955006b1e5f18d6a1226a9
Benno <uth.benno@gmail.com>
2024-03-21 20:26:07 +0100

    Debug

--- scripts/from_scratch_runs/run_docile_5k.sh
+++ scripts/from_scratch_runs/run_docile_5k.sh
@@ -17,18 +17,18 @@ MVLM_CONFIG_PATH=experiments/from_scratch_mvlm_docile_5k.json
 
 # Run MVLM on docile_5k without pretraining
 bash scripts/run_pretraining.sh\
-    MVLM_DATASET=docile_5k\
-    TOKEN_CLASSIF_DATASET=docile\
-    SUBSET_INDEX_PATH=data/docile/subsets_index/5k.json\
-    HOME_DIR=$WORK/DeepInsuranceDocs\
-    MODEL_NAME=layoulm-base-uncased\
-    PRETRAINED_MODEL=null\
-    PREPROCESS_NORMALIZE_TXT=true\
-    PREPROCESS_TAG_SCHEME=BIO\
-    EPOCH_NUM=5\
-    BATCH_SIZE=10\
-    LEARNING_RATE=5e-5\
-    GRADIENT_ACCUMULATION_STEPS=0\
-    MVLM_CONFIG_PATH=experiments/from_scratch_mvlm_docile_5k.json
+    MVLM_DATASET=$MVLM_DATASET\
+    TOKEN_CLASSIF_DATASET=$TOKEN_CLASSIF_DATASET\
+    SUBSET_INDEX_PATH=$SUBSET_INDEX_PATH\
+    HOME_DIR=$HOME_DIR\
+    MODEL_NAME=$MODEL_NAME\
+    PRETRAINED_MODEL=$PRETRAINED_MODEL\
+    PREPROCESS_NORMALIZE_TXT=$PREPROCESS_NORMALIZE_TXT\
+    PREPROCESS_TAG_SCHEME=$PREPROCESS_TAG_SCHEME\
+    EPOCH_NUM=$EPOCH_NUM\
+    BATCH_SIZE=$BATCH_SIZE\
+    LEARNING_RATE=$LEARNING_RATE\
+    GRADIENT_ACCUMULATION_STEPS=$GRADIENT_ACCUMULATION_STEPS\
+    MVLM_CONFIG_PATH=$MVLM_CONFIG_PATH
 
 # Run Token Classif on docile with model pretrained on docile_5k
